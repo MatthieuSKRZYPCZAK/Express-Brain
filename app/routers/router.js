56d61bb605a8a711d0daf7b449fe0761252e6da8
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-04-22 16:49:18 +0200

    Express brain routes

--- app/routers/router.js
+++ app/routers/router.js
@@ -1,14 +1,41 @@
 import express from 'express';
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {
-        title: "EXPRESS BRAINS"
-    })
+    if(req.session && !req.session.goodNumber) {
+        req.session.goodNumber = parseInt(Math.random()*100);
+    }
+    console.log(req.session.goodNumber);
+    const flash = {...req.session.flash}
+    req.session.flash = {};
+    res.render('index', {goodNumber: req.session.goodNumber, flash: flash });
 })
 
-router.post('/mistere', (req, res) => {
-    
+router.post('/', (req, res) => {
+    req.session.flash = {};
+    let playAgain = req.body.playAgain;
+    if(playAgain){
+        req.session.goodNumber = parseInt(Math.random()*100);
+        console.log(req.session.goodNumber);
+        const flash = {...req.session.flash}
+        req.session.flash = {};
+        return res.render('index', {goodNumber: req.session.goodNumber, flash: flash });
+    }
+    if(parseInt(req.body.reponse) > req.session.goodNumber) {
+        req.session.flash.status="danger"
+        req.session.flash.message="Nombre trop grand"
+    } else if(parseInt(req.body.reponse) < req.session.goodNumber) {
+        req.session.flash.status="danger"
+        req.session.flash.message="Nombre trop petit"
+    } else if(parseInt(req.body.reponse) === req.session.goodNumber) {
+        req.session.flash.status="success"
+        req.session.flash.message="Bravo !"
+    } else {
+        req.session.flash.status="danger"
+        req.session.flash.message="Vous devez fournir un nombre"
+    }
+    res.redirect('/');
 })
 
 export default router;
--- index.js
+++ index.js
@@ -2,15 +2,24 @@
 
 import express from 'express';
 import router  from './app/routers/router.js';
+import session from 'express-session';
 const app = express();
 const port = 3000;
+
+app.use(session({
+    secret: 'maCléSecrète',
+    resave: true,
+    saveUninitialized: true
+}));
 
 /* Configuration EJS comme moteur de template */
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
-/* Middlewre pour parser le contenu des requêtes POST */
-app.use(express.urlencoded({extended: true}))
+
+app.use(express.json());
+/* Middleware pour parser le contenu des requêtes POST */
+app.use(express.urlencoded({extended: false}))
 
 /* Fichiers statiques (css, img) */
 app.use(express.static('public'));
